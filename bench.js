9b67730e642127feea7acbb5a85b1e63515bf58b
silverwind <me@silverwind.io>
2022-11-15 21:50:04 +0100

    add sleep

--- bench.js
+++ bench.js
@@ -6,6 +6,7 @@ import axios from "axios";
 
 const urls = Object.keys(pkg.devDependencies).map(name => `https://registry.npmjs.org/${name.replace(/\//g, "%2f")}`);
 const warmupUrls = urls.slice(0, 10);
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 const dispatcher = new UndiciAgent({connections: 96, pipelining: 1});
 const agent = new HttpsAgent({maxSockets: 96, keepAlive: true});
@@ -18,13 +19,19 @@ await Promise.all([
   ...warmupUrls.map(url => undiciFetch(url, {dispatcher}).then(res => res.text())),
 ]);
 
+await sleep(500);
+
 const t3 = performance.now();
 await Promise.all(urls.map(url => axios.get(url, {httpsAgent, responseType: "text"})));
 console.info(`axios: ${Math.round(performance.now() - t3)}ms`);
 
+await sleep(500);
+
 const t2 = performance.now();
 await Promise.all(urls.map(url => nodeFetch(url, {agent}).then(res => res.text())));
 console.info(`node-fetch: ${Math.round(performance.now() - t2)}ms`);
+
+await sleep(500);
 
 const t1 = performance.now();
 await Promise.all(urls.map(url => undiciFetch(url, {dispatcher}).then(res => res.text())));
