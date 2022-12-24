b863c5f6a3afa2de702393efbafd293f1d6bd389
silverwind <me@silverwind.io>
2022-12-24 03:15:26 +0100

    tweaks

--- bench.js
+++ bench.js
@@ -8,6 +8,7 @@ const pkg = JSON.parse(readFileSync(new URL("1500-deps.json", import.meta.url)))
 const urls = Object.keys(pkg.devDependencies).map(name => `https://registry.npmjs.org/${name.replace(/\//g, "%2f")}`);
 const warmupUrls = urls.slice(0, 10);
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+const opts = {concurrency: process.argv[2] ? Number(process.argv[2]) : 96};
 
 // warm up the JIT
 await Promise.all([
@@ -19,17 +20,17 @@ await Promise.all([
 await sleep(500);
 
 const t3 = performance.now();
-await pAll(urls.map(url => () => axios.get(url, {responseType: "text"})), {concurrency: 96});
+await pAll(urls.map(url => () => axios.get(url, {responseType: "text"})), opts);
 console.info(`axios: ${Math.round(performance.now() - t3)}ms`);
 
 await sleep(500);
 
 const t2 = performance.now();
-await pAll(urls.map(url => () => nodeFetch(url).then(res => res.text())), {concurrency: 96});
+await pAll(urls.map(url => () => nodeFetch(url).then(res => res.text())), opts);
 console.info(`node-fetch: ${Math.round(performance.now() - t2)}ms`);
 
 await sleep(500);
 
 const t1 = performance.now();
-await pAll(urls.map(url => () => undiciFetch(url).then(res => res.text())), {concurrency: 96});
+await pAll(urls.map(url => () => undiciFetch(url).then(res => res.text())), opts);
 console.info(`undici: ${Math.round(performance.now() - t1)}ms`);
--- deno.js
+++ deno.js
@@ -4,6 +4,7 @@ import json from "./1500-deps.json" assert {type: "json"}
 const urls = Object.keys(json.devDependencies).map(name => `https://registry.npmjs.org/${name.replace(/\//g, "%2f")}`);
 const warmupUrls = urls.slice(0, 10);
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+const opts = {concurrency: Deno.args[0] ? Number(Deno.args[0]) : 96};
 
 // warm up the JIT
 await Promise.all([
@@ -13,9 +14,7 @@ await Promise.all([
 await sleep(500);
 
 const t1 = performance.now();
-await pAll(urls.map(url => {
-  return () => {
-    return fetch(url).then(res => res.text());
-  }
-}), {concurrency: 96});
+await pAll(urls.map(url => () => {
+  return fetch(url).then(res => res.text());
+}), opts);
 console.info(`deno: ${Math.round(performance.now() - t1)}ms`);
